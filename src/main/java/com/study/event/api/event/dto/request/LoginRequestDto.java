8afb5ab43f1812765eb22e37441de354a6ef504b
Jehoon <limjhoon8@gmail.com>
2024-07-15 10:42:29 +0900

    # 토큰 생성하기

---
+++ src/main/java/com/study/event/api/event/dto/request/LoginRequestDto.java
@@ -0,0 +1,19 @@
+package com.study.event.api.event.dto.request;
+
+import lombok.*;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LoginRequestDto {
+
+    private String email;
+    private String password;
+
+    // 자동 로그인 여부 ...
+
+
+}
